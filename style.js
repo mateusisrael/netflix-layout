9ed827d1a3fbebb1f34933c599ae5ef3676a29f2
João Paulo <joaop.rosa01@gmail.com>
2019-11-08 17:19:29 -0300

    Seletor de usuários. Layout base.

--- style.js
+++ style.js
@@ -1,16 +1,30 @@
 let header = document.querySelector('header');
+let user_cont = document.querySelector('#user-cont');
+let change_user = document.querySelector('#change-user');
+
 
 window.addEventListener('scroll', () => {
     
-    if(document.documentElement.scrollTop > 200) {
+    if(document.documentElement.scrollTop > 70) {
         console.log('1');
         header.style = 'background: black;'
 
         
     }
 
-    if(document.documentElement.scrollTop < 200) {
+    if(document.documentElement.scrollTop < 70) {
         header.style = 'background: linear-gradient(black, transparent);'
 
     }
 })
+
+
+user_cont.addEventListener("mouseover", () => {
+    console.log("over");
+    change_user.style = "display: block;"
+});
+
+user_cont.addEventListener("mouseout", () => {
+        console.log("out");
+        change_user.style = "display: none;"
+});
